4a417078aaa1a72545f8ba1035f9f3dfbf5dc529
Karen <kr@vectra.co.za>
2023-11-30 09:05:06 +0200

    move files

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Karen Rumbie
+ * @copyright Copyright © Karen Rumbie creatives
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'KarenRumbie_AddToCartPopup',
+    __DIR__
+);
